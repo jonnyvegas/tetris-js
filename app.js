459be724e2a69a22034c557717ac2ef9b29f63ac
Jonathan Villegas <jonathan.e.villegas@gmail.com>
2020-08-27 08:48:32 -0700

    Tetrominos are in and appear on screen in a simple test.

--- app.js
+++ app.js
@@ -7,6 +7,55 @@ document.addEventListener('DOMContentLoaded', () => {
 	
 	//console.log(squares);
 	
+	// Tetromino
+	const lTetrimino = [
+		[1, width+ 1, width * 2 + 1, 2],
+		[width, width + 1, width + 2, width * 2 + 2],
+		[1, width + 1, width * 2 + 1, width * 2],
+		[width, width * 2, width * 2 + 1, width*2+2]
+	]
+	
+	const zTetromino = [
+	[0, width, width + 1, width * 2 + 1],
+	[width + 1, width + 2, width * 2, width * 2 + 1],
+	[0, width, width + 1, width * 2 + 1],
+	[width + 1, width + 2, width * 2, width * 2 + 1]
+	]
+	
+	const tTetromino = [
+	[1, width, width + 1, width + 2],
+	[1, width + 1, width + 2, width * 2 + 1],
+	[width, width + 1, width + 2, width * 2 + 1],
+	[1, width, width + 1, width * 2 + 1]
+	]
+	
+	const oTetromino = [
+	[0, 1, width, width + 1],
+	[0, 1, width, width + 1],
+	[0, 1, width, width + 1],
+	[0, 1, width, width + 1]
+	]
+	
+	const iTetromino = [
+	[1, width + 1, width * 2 + 1, width * 3 + 1],
+	[width, width + 1, width + 2, width + 3],
+	[1, width + 1, width * 2 + 1, width * 3 + 1],
+	[width, width + 1, width + 2, width + 3]
+	]
 	
 	
+	const theTetrominos = [lTetrimino, zTetromino, tTetromino, oTetromino, iTetromino];
+	
+	let currentPosition = 4;
+	let current = theTetrominos[0][0];
+	//console.log(current);
+	
+	// draw first rotation in tetromino
+	function draw(){
+		current.forEach(index => {
+			squares[currentPosition + index].classList.add('tetromino');
+		})
+	}
+	
+	draw();
 })
